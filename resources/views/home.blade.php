9dbdf5a3456c88b94efe7e5645c35892b27bb770
Cybee45 <cyberswat45@gmail.com>
2025-05-07 15:25:05 +0800

    awang

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged AWANG!') }}
                 </div>
             </div>
         </div>
